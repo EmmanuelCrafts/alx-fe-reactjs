d2cbc613bf863444489145e797e32dea31d8a87b
Emmanuel Oginga <ogolemmanuel3@gmail.com>
2025-07-27 23:48:41 +0300

    refactor: implemented Context API to manage userData across components and removed prop drilling

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,20 +7,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import UserProfile from './components/UserProfile';
-import { Counter } from './components/Counter';
-import { userContext } from './components/UserContext';
-import ProfilePage from './components/ProfilePage';
+import { Counter
+ } from './components/Counter';
+
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
   return (
       <div>
         <WelcomeMessage />
         <Header />
         <MainContent />
         <Counter />
-          <UserContext.Provider value={userData}>
-             <ProfilePage />
-          </UserContext.Provider>
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
         <Footer />
       </div>
---
+++ alx-react-app-props/src/components/MainContent.jsx
@@ -0,0 +1,7 @@
+export default function MainContent(){
+   return(
+    <main style={{backgroundColor: 'lightgray', color: 'black'}}>
+  <p>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+   );
+}
