e66ad42100207514bc1be739b8bf3b9cf444a756
Emmanuel Oginga <ogolemmanuel3@gmail.com>
2025-08-03 16:27:40 +0300

    feat :create EditForm component that allows user to update the recipe

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import useRecipeStore from "./recipeStore";
+import EditRecipeForm from "./EditRecipeForm";
 
 const RecipeDetails = () => {
     const {id} = useParams();
@@ -8,6 +9,7 @@ const RecipeDetails = () => {
         <div>
             <h1>{recipe.title}</h1>
             <p>{recipe.description}</p>
+            <EditRecipeForm recipe={recipe} />
         </div>
     );
 };
