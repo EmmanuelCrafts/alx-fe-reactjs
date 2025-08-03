b3d0f5e56598ecbe76484bc2a54ec390b3e0b002
Emmanuel Oginga <ogolemmanuel3@gmail.com>
2025-08-03 04:42:20 +0300

    feat: create addRecipeForm component

---
+++ recipe-sharing-app/src/components/AddRecipeForm.jsx
@@ -0,0 +1,27 @@
+import useRecipeStore from "../store/useRecipeStore";
+import { useState } from "react";
+
+const AddRecipeForm = () => {
+    const addRecipe = useRecipeStore(state => state.addRecipe);
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        addRecipe({id: Date.now(),title,description});
+        setTitle('');
+        setDescription('');
+    };
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <input
+             type="text"
+             value={title}
+             onChange={(e) => setTitle(e.target.value)}
+             placeholder="Description"
+             />
+             <button type="submit">Add Recipe</button>
+        </form>
+    );
+};
