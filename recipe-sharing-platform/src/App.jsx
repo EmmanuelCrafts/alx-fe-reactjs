13e217f91aca556a7debbb7af617bd2c542bcd79
Emmanuel Oginga <ogolemmanuel3@gmail.com>
2025-08-13 17:59:16 +0300

    feat:style homepage and make it responsive

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,12 +1,9 @@
-import './App.css'
-
+import RecipeFinder from './components/HomePage'
 function App() {
 
   return (
     <>
-     <div>
-      <p className='text-blue-500'>i love tennis</p>
-      </div>
+    <RecipeFinder />
     </>
   )
 }
--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect} from "react";
+ import { useState, useEffect} from "react";
 import data from "../data.json"
 function RecipeFinder(){
     const[recipes, setRecipes] = useState([]);
@@ -8,13 +8,25 @@ function RecipeFinder(){
     }, []);
 
     return (
-       <div>
+       <div className="h-auto  mt-4 mx-2  relative ">
+             <h1 className="font-extrabold text-2xl mb-2 text-center text-orange-500 hover:text-orange-600
+              transition duration-100 ">
+              My Recipes</h1>
        {recipes.map(recipe => (
-        <div key={recipe.id}>
-             <h1>{recipe.title}</h1>
-             <p>{recipe.summary}</p>
+        <div key={recipe.id} className="mb-8  pt-4 pb-4 px-4 flex flex-row-reverse  gap-4 bg-gray-100 hover:scale-105 
+        transition duration-300 ease-in-out shadow-lg md:w-[80%] md:mx-auto lg:w-[50%] ">
+          <div>
+          <h2 className="font-medium text-orange-400  text-lg">{recipe.title}</h2>
+          <p className="text-left">{recipe.summary}</p>
+          </div>
+             <img 
+             src={recipe.image} 
+             alt="recipe" 
+             className="w-1/2 md:w-1/3 lg:w-1/4 rounded-2xl"
+             />
         </div>
        ))}
        </div>
     );
 }
+export default RecipeFinder;
