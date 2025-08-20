69db9d6838c2d0ef73cbbde6e6fd4919e61dd1b8
Emmanuel Oginga <ogolemmanuel3@gmail.com>
2025-08-20 21:13:26 +0300

    create a form using controlled components

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -12,4 +12,57 @@ const RegistrationForm = () => {
             ...prev, [name] : value
         }));
     };
+    
+    const validateForm = () => {
+        const { username, email, password } = FormData;
+        if (!username || !email || !password) {
+            alert("All fields are required");
+            return false;
+        }
+        return true;       
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!validateForm()) {
+            return;
+        }
+
+       alert("Registration successful");
+        SetFormData({
+            username: "",
+            email: "",
+            password: "",
+        });
+    }
+
+    return (
+        <div>
+          <form onSubmit={handleSubmit}>
+              <input 
+              type="text"
+              name="username"
+              value={FormData.username}
+              onChange={handleChange}
+              placeholder="Enter username"
+               />
+             <input
+              type="email" 
+              name="email" 
+              value={FormData.email}
+              placeholder="enter email"
+              onChange={handleChange}
+               />
+               <input
+                type="password" 
+                name="password" 
+                value={FormData.password}
+                onChange={handleChange}
+                placeholder="enter password"
+                 />
+
+                <button type="submit">Register</button>
+          </form>
+            
+        </div>
+    )
 }
