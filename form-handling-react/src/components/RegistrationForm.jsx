c71fac6692556316af9488cd42a5309d098f5126
Emmanuel Oginga <ogolemmanuel3@gmail.com>
2025-08-20 22:45:24 +0300

    add spacing between if and ()

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -18,9 +18,9 @@ const RegistrationForm = () => {
     
     const validateForm = () => {
         const newErrors = {};
-        if(!username) errors.username = "Username is required";
-        if(!email) errors.email = "Email is required";
-        if(!password) errors.password = "Password is required";
+        if (!username) errors.username = "Username is required";
+        if (!email) errors.email = "Email is required";
+        if (!password) errors.password = "Password is required";
        
         SetErrors(newErrors);
         return Object.keys(newErrors).length === 0;
